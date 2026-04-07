62093a39e07f66e66bdb8fe96c28b9f8e7576104
Vaibhav.Pawar <vaibhav.pawar@macOS-Admins-MacBook-Pro.local>
2026-04-07 22:24:29 +0530

    feat: add AuthResponse DTO for JWT token response

---
+++ src/main/java/com/jobboard/auth/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.jobboard.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+}
